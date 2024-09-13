c90526df33eac1ac4e2dabd1ba2626fc88d50f1e
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-13 20:09:55 +0700

    test-1

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express'
 
 const app = express()
-// Định nghĩa tuyến đường cơ bản
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Lắng nghe trên cổng 3000
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
